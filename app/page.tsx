c625cbd32c3f28e1600426cf1e4529fc84873956
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2023-01-08 20:49:23 +0700

    feat: add gray matter package to parse metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,34 @@
 import fs from 'fs';
+import matter from 'gray-matter';
 import Link from 'next/link';
 
-const getPostMetaData = () => {
+import { PostMetaData } from '../models/PostMetaData';
+
+const getPostMetaData = (): PostMetaData[] => {
     const folder = 'posts/';
     const files = fs.readdirSync(folder);
     const markdownPosts = files.filter((file) => file.endsWith('.md'));
-    const slugs = markdownPosts.map((file) => file.replace('.md', ''));
 
-    return slugs;
+    const posts = markdownPosts.map((fileName) => {
+        const fileContent = fs.readFileSync(`posts/${fileName}`, 'utf8');
+        const matterResult = matter(fileContent);
+
+        return {
+            title: matterResult.data.title,
+            difficult: matterResult.data.difficult,
+            slug: fileName.replace('.md', ''),
+        };
+    });
+
+    return posts;
 };
 
 const Homepage = () => {
     const postMetaData = getPostMetaData();
-    const postPreviews = postMetaData.map((slug, index) => (
+    const postPreviews = postMetaData.map((post, index) => (
         <div key={index}>
-            <Link href={`/posts/${slug}`}>{slug}</Link>
+            <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+            <p>{post.difficult}</p>
         </div>
     ));
 
