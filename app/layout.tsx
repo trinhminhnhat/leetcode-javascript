31c660a7737265990fb14b80396199c2f663a8a4
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2023-01-09 20:23:32 +0700

    feat: install tailwindcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import "styles/globals.css";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     const header = (
         <header>
